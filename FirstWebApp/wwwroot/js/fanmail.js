934aab575f21cd3c39bbf4867d1353cd97634bd5
maxfi <maxfi@CMAX_LAPTOP>
2022-01-08 14:22:55 -0700

    Add project files.

---
+++ FirstWebApp/wwwroot/js/fanmail.js
@@ -0,0 +1,8 @@
+﻿$("#send_button").click(function () {
+    from = $("#text_from").val();
+    subject = $("#text_subject").val();
+    message = $("#text_message").val();
+    final_message = from + " says this: " + subject + " \n" + message;
+    alert(final_message);
+    $("#g_pic").fadeOut("slow");
+});
